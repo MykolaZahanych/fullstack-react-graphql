3193c56a2d206f1c27a7ca0237e543c2282bb94e
Mykola Zahanych <kolyazaganych98@gmail.com>
2020-09-19 18:59:22 +0200

    delete singlePost

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../utils/createUrqlClient';
-import { usePostsQuery } from '../generated/graphql';
+import { useDeletePostMutation, usePostsQuery } from '../generated/graphql';
 import { Layout } from '../components/Layout';
 import { Heading, Link, Stack, Box, Text, Flex, Button, IconButton } from '@chakra-ui/core';
 import NextLink from 'next/link';
@@ -12,6 +12,7 @@ const Index = () => {
   const [{ data, fetching }] = usePostsQuery({
     variables,
   });
+  const [, deletePost] = useDeletePostMutation();
 
   if (!fetching && !data) {
     return <div>You got no post, some error!_!</div>;
@@ -23,20 +24,34 @@ const Index = () => {
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data!.posts.posts.map((p) => (
-            <Flex key={p.id} p={5} shadow='md' borderWidth='1px'>
-              <UpdootSection post={p} />
-              <Box>
-                <NextLink href='post/[id]' as={`/post/${p.id}`}>
-                  <Link>
-                    <Heading fontSize='xl'>{p.title}</Heading>
-                  </Link>
-                </NextLink>
-                <Text>posted by: {p.creator.username}</Text>
-                <Text mt={4}>{p.textSnippet.slice(0, 50)}</Text>
-              </Box>
-            </Flex>
-          ))}
+          {data!.posts.posts.map((p) =>
+            !p ? null : (
+              <Flex key={p.id} p={5} shadow='md' borderWidth='1px'>
+                <UpdootSection post={p} />
+                <Box flex={1}>
+                  <NextLink href='post/[id]' as={`/post/${p.id}`}>
+                    <Link>
+                      <Heading fontSize='xl'>{p.title}</Heading>
+                    </Link>
+                  </NextLink>
+                  <Text>posted by: {p.creator.username}</Text>
+                  <Flex align='center'>
+                    <Text flex={1} mt={4}>
+                      {p.textSnippet.slice(0, 50)}
+                    </Text>
+                    <IconButton
+                      variantColor='red'
+                      icon='delete'
+                      aria-label='Delete Post'
+                      onClick={() => {
+                        deletePost({ id: p.id });
+                      }}
+                    />
+                  </Flex>
+                </Box>
+              </Flex>
+            )
+          )}
         </Stack>
       )}
       {data && data.posts.hasMore ? (
